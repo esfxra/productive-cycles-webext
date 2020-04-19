819ab903e827d162727b2cbdde4ebc2124ebada0
Diego Serrano <diegoserrano@diegos-mbp.home>
2020-04-18 23:44:21 -0400

    prototype for UI updater and storage operations

--- background-timer.js
+++ background-timer.js
@@ -1,4 +1,6 @@
 let port = null;
+let defaultTime = 1 * 60000;
+let timerRunning = false;
 
 chrome.runtime.onConnect.addListener((portFromPopUp) => {
   port = portFromPopUp;
@@ -7,6 +9,32 @@ chrome.runtime.onConnect.addListener((portFromPopUp) => {
 
 function handleInput(message) {
   if (message.command === "start") {
-    port.postMessage(Date.now());
+    // port.postMessage(Date.now());
+
+    // Start the counter
+    // 1. Get current time
+    let time = Date.now();
+    let target = time + defaultTime;
+
+    // 2. Save the time in the browser's local storage
+    chrome.storage.local.set({endTime: target}, function() {
+      console.log("Stored endTime:", target);
+      // console.log('Remaining:', target - time);
+    });
+
+    // 3. Set an alarm to notify the user in "defaultTime"
+    // chrome.alarms.create("cycleAlarm", {when: target});
+
+    // 4. Set a flag that the timer has started
+    timerRunning = true;
+  }
+  else if (message.command === "unload") {
+    console.log("unloaded")
   }
 }
+
+// chrome.alarms.onAlarm.addListener( () => alert("hey! 1 minute passed"));
+
+// chrome.storage.local.get(['key'], function(result) {
+//   console.log('Value currently is ' + result.key);
+// });
--- popup/timer.js
+++ popup/timer.js
@@ -1,4 +1,5 @@
 let port = chrome.runtime.connect({ name: "port-from-popup" });
+let interval = null;
 
 document.addEventListener("click", (e) => {
   let selection = e.target.id;
@@ -6,6 +7,37 @@ document.addEventListener("click", (e) => {
 });
 
 port.onMessage.addListener((message) => document.querySelector("#time").textContent = message);
+
+window.addEventListener("DOMContentLoaded", (event) => {
+
+  let target = null;
+  let remaining = null;
+  let seconds = null;
+
+  // 1. Request time from storage
+  chrome.storage.local.get(["endTime"], (result) => {
+    target = result.endTime;
+    console.log("target time:", target);
+
+    // 2. Calculate time left (every second)
+    // This assumes setInterval() will be cleared once the popup is closed
+    // Consider moving the interval to the background script AND just running when there is a live connection
+    interval = setInterval(() => {
+      remaining = target - Date.now();
+      seconds = remaining / 1000;
+
+      if (seconds < 0) {
+        seconds = 0;
+        clearInterval(interval);
+        // alert("hey!"); // for debugging
+      }
+
+      // 3. Update the time left in #time
+      document.querySelector("#time").textContent = seconds;
+
+    }, 1000);
+  });
+});
 
 
 
