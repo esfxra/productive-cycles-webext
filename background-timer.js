8e76a3126c91f34bfd3332d51a1bc46f247feffe
Diego Serrano <diegoserrano@diegos-mbp.home>
2020-04-19 20:09:05 -0400

    interval function moved to background-timer

--- background-timer.js
+++ background-timer.js
@@ -1,40 +1,148 @@
+"use strict";
+
+// background-timer.js globals
 let port = null;
-let defaultTime = 1 * 60000;
-let timerRunning = false;
+let uiUpdater = null;
+let popUpOpen = false;
+
+// Defaults
+let defaultTime = 25 * 60000;
+let defaultCycles = 4;
+
+// Timer object
+var Timer = {
+  target: null,
+  remaining: defaultTime, // Time remaining
+  cycle: 1,
+  totalCycles: defaultCycles, // Will replace with a number read from file / storage / etc
+  status: "initial",
+  remainingStr: function () {
+    // Use a time library for better ms-to-minutes-seconds in the future
+    let min = Math.floor((this.remaining) % (1000 * 60 * 60) / (1000 * 60));
+    let sec = Math.ceil((this.remaining) % (1000 * 60) / 1000);
+
+    // Temporary fix for -1 at the end of timer
+    if (min < 1) { min = 0; }
+    if (sec < 1) { sec = 0; }
+
+    let minStr = "";
+    let secStr = "";
+
+    // Format processed time; add missing 0s
+    if (Math.floor(Math.log10(min)) < 1) { minStr = "0" + min; }
+    else { minStr = min; }
+
+    if (Math.floor(Math.log10(sec)) < 1) { secStr = "0" + sec; }
+    else { secStr = sec; }
+
+    return minStr + ":" + secStr;
+  },
+};
+
+chrome.runtime.onInstalled.addListener((details) => {
+  // need to add if statements for install or update
+  chrome.storage.local.set({
+    target: null,
+    status: "initial"
+  }, () => console.log("OnInstalled config for target and status"));
+})
 
 chrome.runtime.onConnect.addListener((portFromPopUp) => {
   port = portFromPopUp;
+  popUpOpen = true;
+
   port.onMessage.addListener(handleInput);
+
+  // Experimental: This seems to be the best place to ...
+  // update target after the popup has gone inactive ...
+  // IF this does not work ... move it to preload ... case === "running" ...
+  // OR move it to the initialization of the Object ... and specifiy default value too
+  chrome.storage.local.get(["target", "status"], (result) => {
+    Timer.target = result.target;
+    Timer.status = result.status;
+    console.log(Timer.target);
+  });
+
+  port.onDisconnect.addListener(() => {
+    popUpOpen = false;
+    clearInterval(uiUpdater);
+    console.log("PopUp port disconnected; interval cleared");
+  })
 });
 
 function handleInput(message) {
   if (message.command === "start") {
-    // port.postMessage(Date.now());
 
-    // Start the counter
-    // 1. Get current time
-    let time = Date.now();
-    let target = time + defaultTime;
+    if (Timer.status === "initial") {
+      // The current time + what the timer is set up to count as a cycle
+      let startTime = Date.now();
+      Timer.target = startTime + defaultTime;
 
-    // 2. Save the time in the browser's local storage
-    chrome.storage.local.set({endTime: target}, function() {
-      console.log("Stored endTime:", target);
-      // console.log('Remaining:', target - time);
-    });
+      // The amount of milliseconds needed to reach Timer.target
+      // This is the same amount as defaultTime
+      Timer.remaining = Timer.target - startTime;
 
-    // 3. Set an alarm to notify the user in "defaultTime"
-    // chrome.alarms.create("cycleAlarm", {when: target});
+      // The status of the timer
+      Timer.status = "running";
 
-    // 4. Set a flag that the timer has started
-    timerRunning = true;
+      console.log("Timer.target:", Timer.target);
+      console.log("Timer.remaining:", Timer.remaining);
+      console.log("Timer.status:", Timer.status);
+
+      chrome.storage.local.set({
+        target: Timer.target,
+        status: Timer.status
+      }, () => console.log("Timer target and status (running) saved"));
+
+      // Set an alarm
+      // chrome.alarms.create( ... );
+    }
+
+    uiUpdater = setInterval(updateUI, 1000);
   }
-  else if (message.command === "unload") {
-    console.log("unloaded")
+  else if (message.command === "preload") {
+    if (Timer.status === "initial") {
+      port.postMessage(Timer.remainingStr());
+    }
+    else if (Timer.status === "running") {
+      Timer.remaining = Timer.target - Date.now();
+      console.log("Time left (ms)", Timer.remaining)
+      port.postMessage(Timer.remainingStr());
+
+      uiUpdater = setInterval(updateUI, 1000);
+    }
   }
 }
 
-// chrome.alarms.onAlarm.addListener( () => alert("hey! 1 minute passed"));
+function updateUI() {
+  // Make calculations to find remaining time (ms to minutes-seconds)
+  Timer.remaining = Timer.remaining - 1000;
+  let time = Timer.remainingStr();
 
-// chrome.storage.local.get(['key'], function(result) {
-//   console.log('Value currently is ' + result.key);
+  if (popUpOpen) {
+    console.log("Time posted:", time);
+    port.postMessage(time);
+  }
+
+  if (time === "00:00" || !popUpOpen) {
+    clearInterval(uiUpdater);
+  }
+
+}
+
+// chrome.alarms.onAlarm.addListener(() => {
+//   Timer.status = "complete";
+
+//   // Clear the interval from here ... Need to test this ... Since the interval is started by another listener
+//   // Also, this code will only run if the popup view is open ??
+//   clearInterval(uiUpdater);
+
+//   // Notify the user
+//   let notificationID = "cycle-complete-alarm";
+//   chrome.notifications.create(notificationID, {
+//     "type": "basic",
+//     "iconUrl": chrome.runtime.getURL("icons/time-512.png"),
+//     "title": "cycle complete!",
+//     "message": "everyone, take 5"
+//   });
 // });
--- popup/timer.js
+++ popup/timer.js
@@ -1,59 +1,20 @@
-let port = chrome.runtime.connect({ name: "port-from-popup" });
-let interval = null;
+"use strict";
 
+// timer.js globals
+let port = chrome.runtime.connect({ name: "port-from-popup" });
+let status = null;
+
+// Handle inputs
 document.addEventListener("click", (e) => {
   let selection = e.target.id;
-  port.postMessage({ command: selection })
+  port.postMessage({ command: selection });
 });
 
-port.onMessage.addListener((message) => document.querySelector("#time").textContent = message);
-
+// Register the UI has been loaded and let the background script know
 window.addEventListener("DOMContentLoaded", (event) => {
-
-  let target = null;
-  let remaining = null;
-  let seconds = null;
-
-  // 1. Request time from storage
-  chrome.storage.local.get(["endTime"], (result) => {
-    target = result.endTime;
-    console.log("target time:", target);
-
-    // 2. Calculate time left (every second)
-    // This assumes setInterval() will be cleared once the popup is closed
-    // Consider moving the interval to the background script AND just running when there is a live connection
-    interval = setInterval(() => {
-      remaining = target - Date.now();
-      seconds = remaining / 1000;
-
-      if (seconds < 0) {
-        seconds = 0;
-        clearInterval(interval);
-        // alert("hey!"); // for debugging
-      }
-
-      // 3. Update the time left in #time
-      document.querySelector("#time").textContent = seconds;
-
-    }, 1000);
-  });
+  console.log(event);
+  port.postMessage({ command: "preload" });
 });
 
-
-
-
-
-
-// chrome.storage.sync.set({ startTime: Date.now()}, () => console.log("Current time saved."));
-
-// window.addEventListener("load", () => {
-//     // Get time saved in storage, and subtract from current time
-//     chrome.storage.sync.get(["startTime"], (result) => {
-//         let startTime = result.startTime;
-//         let currentTime = Date.now();
-//         console.log("start: ", startTime, "current: ", currentTime);
-//         console.log("difference: ", startTime - currentTime);
-
-//         document.querySelector("#time").textContent = startTime - currentTime;
-//     });
-// });
+// Change the text in the #time element with the updated time coming from the background script
+port.onMessage.addListener( (message) => document.querySelector("#time").textContent = message );
