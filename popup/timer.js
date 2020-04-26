def46302760733d0255630732b47bda6317d7092
Diego Serrano <diegoserrano@diegos-mbp.home>
2020-04-26 16:23:19 -0400

    version bump, tracker replaced innerHTML with nodes

--- popup/timer.js
+++ popup/timer.js
@@ -2,6 +2,8 @@
 
 // timer.js globals
 var port = chrome.runtime.connect({ name: "port-from-popup" });
+var statusChanged = false;
+var previousStatus = null;
 
 // Handle inputs
 document.addEventListener("click", (e) => {
@@ -33,12 +35,18 @@ window.addEventListener("DOMContentLoaded", (event) => {
 port.onMessage.addListener((message) => {
   console.log(message);
 
+  // Check if there is a change in status
+  // Note: case "pause" does not send updated "paused" status to timer.js until after UI is opened again
+  // That is when preload command runs again
+  if (previousStatus !== message.status) { statusChanged = true; previousStatus = message.status; }
+  else { statusChanged = false; }
+
   // Change the text in the #time element with the updated time coming from the background script
   document.querySelector("#time").textContent = message.time;
 
   // Check if the timer is complete, and change time text to "complete"
   // if (message.status === "complete" || message.cycle > message.totalCycles) {
-    if (message.status === "complete") {
+  if (message.status === "complete") {
 
     document.querySelector("#time").textContent = "complete";
 
@@ -71,28 +79,49 @@ port.onMessage.addListener((message) => {
 
   // Tracker
   // Compute the number of cycles to display, which one is running, and which are complete
-  let html = '';
-  let i = 1;
-  while (i <= message.totalCycles) {
-    if (i === message.cycle) {
-      if (message.status === "initial") {
-        html += '<span id="cycle-' + i + '" class="dot pending"></span>';
-      }
-      else if (message.status === "running" || message.status === "paused") {
-        html += '<span id="cycle-' + i + '" class="dot running"></span>';
-      }
-    }
-    else if (i < message.cycle) {
-      html += '<span id="cycle-' + i + '" class="dot complete"></span>';
-    }
-    else {
-      // This should affect all cycles that are past the current (i.e. i > message.cycle)
-      html += '<span id="cycle-' + i + '" class="dot pending"></span>';
-    }
-    i++;
-  }
+  if (statusChanged) {
+    console.log("Rebuilding the tracker ...")
+    console.log(previousStatus);
+    let cyclesNode = document.querySelector(".cycles");
 
-  document.querySelector(".cycles").innerHTML = html;
+    // Reset cyclesNode
+    let node = cyclesNode.lastElementChild;
+    while (node) {
+      cyclesNode.removeChild(node);
+      node = cyclesNode.lastElementChild;
+    }
+
+    // Build cyclesNode
+    let dotNode = null;
+    let i = 1;
+    while (i <= message.totalCycles) {
+      dotNode = document.createElement("span");
+      dotNode.id = "cycle-" + i;
+      dotNode.classList.add("dot");
+      if (i === message.cycle) {
+        if (message.status === "initial") {
+          dotNode.classList.add("pending");
+          // html += '<span id="cycle-' + i + '" class="dot pending"></span>';
+        }
+        else if (message.status === "running" || message.status === "paused") {
+          dotNode.classList.add("running");
+          // html += '<span id="cycle-' + i + '" class="dot running"></span>';
+        }
+      }
+      else if (i < message.cycle) {
+        dotNode.classList.add("complete");
+        // html += '<span id="cycle-' + i + '" class="dot complete"></span>';
+      }
+      else {
+        // This should affect all cycles that are past the current (i.e. i > message.cycle)
+        dotNode.classList.add("pending");
+        // html += '<span id="cycle-' + i + '" class="dot pending"></span>';
+      }
+      cyclesNode.appendChild(dotNode);
+      i++;
+    }
+    // document.querySelector(".cycles").innerHTML = html;
+  }
 });
 
 function switchButtons(hide, show) {
