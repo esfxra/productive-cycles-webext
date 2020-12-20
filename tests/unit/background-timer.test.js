fce626c8af6dea869fa787acf746921be58bbca7
Diego Serrano <diegoserranor@gmail.com>
2020-12-20 15:56:23 -0500

    Add testing for sync, correct subtractor tests

--- tests/unit/background-timer.test.js
+++ tests/unit/background-timer.test.js
@@ -81,14 +81,6 @@ describe('Timeline', () => {
 });
 
 describe('Subtractor', () => {
-  const subtractSeconds = (seconds) => {
-    let i = 1;
-    while (i <= seconds) {
-      jest.advanceTimersByTime(1000);
-      i += 1;
-    }
-  };
-
   let timer;
   let runSubtractor;
   let postState;
@@ -104,31 +96,13 @@ describe('Subtractor', () => {
     jest.useFakeTimers();
   });
 
-  test('Subtracts 1 second immediately after call', () => {
-    timer.state.time = 10000;
-    timer.runSubtractor();
-    expect(timer.state.time).toBe(9000);
-  });
-
   test('Subtracts seconds according to time passed', () => {
-    timer.state.time = 10000;
+    const seconds = 10;
+    timer.state.time = seconds * 1000;
     timer.runSubtractor();
 
-    expect(timer.state.time).toBe(9000);
-
-    subtractSeconds(4);
-    expect(timer.state.time).toBe(5000);
-
-    subtractSeconds(5);
+    jest.advanceTimersByTime(seconds * 1000);
     expect(timer.state.time).toBe(0);
-  });
-
-  test('Posts state 1 time after 1st subtraction', () => {
-    timer.state.time = 1000;
-    timer.runSubtractor();
-
-    // ONLY takes into account the call outside the interval
-    expect(postState).toHaveBeenCalledTimes(1);
   });
 
   test('Posts state according to time passed', () => {
@@ -136,9 +110,8 @@ describe('Subtractor', () => {
     timer.state.time = seconds * 1000;
     timer.runSubtractor();
 
-    subtractSeconds(seconds);
+    jest.advanceTimersByTime(seconds * 1000);
 
-    // ALSO takes into account the call outside the interval
     expect(postState).toHaveBeenCalledTimes(seconds);
   });
 
@@ -159,7 +132,10 @@ describe('Subtractor', () => {
     timer.state.time = seconds * 1000;
     timer.runSubtractor();
 
-    subtractSeconds(seconds);
+    jest.advanceTimersByTime(seconds * 1000);
+    expect(clearInterval).toHaveBeenCalledTimes(0);
+
+    jest.advanceTimersByTime(1000);
     expect(clearInterval).toHaveBeenCalledTimes(1);
   });
 
@@ -190,12 +166,8 @@ describe('Subtractor', () => {
 
 describe('State Operations', () => {
   let timer;
-  beforeAll(() => {
-    timer = new Timer(defaultValues);
-  });
-  beforeEach(() => {
-    timer.resetAll();
-  });
+  beforeAll(() => (timer = new Timer(defaultValues)));
+  beforeEach(() => timer.resetAll());
 
   test('Can retrieve the current state with getState', () => {
     timer.state.period = 3;
@@ -261,9 +233,7 @@ describe('State Operations', () => {
 
 describe('Comms', () => {
   let timer;
-  beforeAll(() => {
-    timer = new Timer(defaultValues);
-  });
+  beforeAll(() => (timer = new Timer(defaultValues)));
 
   test('Port is initialized as closed', () => {
     expect(timer.comms.portOpen).toBe(false);
@@ -278,5 +248,192 @@ describe('Comms', () => {
     // Using toBe because we want the identity to be the same
     expect(timer.comms.port).toBe(testPort);
     expect(timer.comms.portOpen).toBe(true);
+  });
+});
+
+describe('Sync', () => {
+  const randomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  let timer;
+  beforeAll(() => (timer = new Timer(defaultValues)));
+  beforeEach(() => timer.resetAll());
+
+  test.each([['running'], ['break']])('Syncs if status is %s', (status) => {
+    timer.state.status = status;
+    const result = timer.sync();
+    expect(result).toBe(true);
+  });
+
+  test.each([['initial'], ['paused'], ['complete']])(
+    'Does not sync if status is %s',
+    (status) => {
+      timer.state.status = status;
+      const result = timer.sync();
+      expect(result).toBe(false);
+    }
+  );
+
+  describe('With auto-start', () => {
+    test('Corrects the state from an arbitrary period to timer complete', () => {
+      timer.state.period = randomInt(0, 6);
+      timer.buildTimeline();
+      timer.state.status = timer.state.period % 2 === 0 ? 'running' : 'break';
+
+      const reference = timer.timeline[6] + 1000;
+      timer.sync(reference);
+
+      expect(timer.state.period).toBe(7);
+      expect(timer.state.time).toBe(0);
+      expect(timer.state.status).toBe('complete');
+    });
+
+    describe('Cycle to Cycle corrections', () => {
+      test.each([
+        [0, 0],
+        [0, 2],
+        [0, 4],
+        [0, 6],
+        [2, 2],
+        [2, 4],
+        [2, 6],
+        [4, 4],
+        [4, 6],
+        [6, 6],
+      ])(
+        'Corrects the state from period %i (cycle) to period %i (cycle)',
+        (a, b) => {
+          timer.state.period = a;
+          timer.state.time = timer.settings.cycleTime;
+          timer.state.status = 'running';
+          timer.buildTimeline();
+
+          const reference = timer.timeline[b] - 1000;
+          timer.sync(reference);
+
+          expect(timer.state.period).toBe(b);
+          expect(timer.state.status).toBe('running');
+
+          expect(timer.state.time).toBeLessThanOrEqual(1000);
+          expect(timer.state.time).toBeGreaterThanOrEqual(0);
+        }
+      );
+    });
+
+    describe('Break to Break corrections', () => {
+      test.each([
+        [1, 1],
+        [1, 3],
+        [1, 5],
+        [3, 3],
+        [3, 5],
+        [5, 5],
+      ])(
+        'Corrects the state from period %i (break) to period %i (break)',
+        (a, b) => {
+          timer.state.period = a;
+          timer.state.time = timer.settings.breakTime;
+          timer.state.status = 'break';
+          timer.buildTimeline();
+
+          const reference = timer.timeline[b] - 1000;
+          timer.sync(reference);
+
+          expect(timer.state.period).toBe(b);
+          expect(timer.state.status).toBe('break');
+
+          expect(timer.state.time).toBeLessThanOrEqual(1000);
+          expect(timer.state.time).toBeGreaterThanOrEqual(0);
+        }
+      );
+    });
+
+    describe('Cycle to Break corrections', () => {
+      test.each([
+        [0, 1],
+        [0, 3],
+        [0, 5],
+        [2, 3],
+        [2, 5],
+        [4, 5],
+      ])(
+        'Corrects the state from period %i (cycle) to period %i (break)',
+        (a, b) => {
+          timer.state.period = a;
+          timer.state.time = timer.settings.cycleTime;
+          timer.state.status = 'running';
+          timer.buildTimeline();
+
+          const reference = timer.timeline[b] - 1000;
+          timer.sync(reference);
+
+          expect(timer.state.period).toBe(b);
+          expect(timer.state.status).toBe('break');
+
+          expect(timer.state.time).toBeLessThanOrEqual(1000);
+          expect(timer.state.time).toBeGreaterThanOrEqual(0);
+        }
+      );
+    });
+
+    describe('Break to Cycle corrections', () => {
+      test.each([
+        [1, 2],
+        [1, 4],
+        [1, 6],
+        [3, 4],
+        [3, 6],
+        [5, 6],
+      ])(
+        'Corrects the state from period %i (break) to period %i (cycle)',
+        (a, b) => {
+          timer.state.period = a;
+          timer.state.time = timer.settings.breakTime;
+          timer.state.status = 'break';
+          timer.buildTimeline();
+
+          const reference = timer.timeline[b] - 1000;
+          timer.sync(reference);
+
+          expect(timer.state.period).toBe(b);
+          expect(timer.state.status).toBe('running');
+
+          expect(timer.state.time).toBeLessThanOrEqual(1000);
+          expect(timer.state.time).toBeGreaterThanOrEqual(0);
+        }
+      );
+    });
+  });
+
+  describe('Without auto-start', () => {
+    beforeAll(() => (timer.settings.autoStart = false));
+    test('Corrects the time when still within the same period', () => {
+      timer.state.period = 1;
+      timer.state.time = timer.settings.breakTime;
+      timer.state.status = 'break';
+      timer.buildTimeline();
+
+      const target = timer.timeline[timer.state.period] - 1000;
+      timer.sync(target);
+
+      expect(timer.state.period).toBe(1);
+      expect(timer.state.time).toBe(1000);
+      expect(timer.state.status).toBe('break');
+    });
+
+    test('Ends the current period when the corrected time is less than 0', () => {
+      timer.state.period = 1;
+      timer.state.time = timer.settings.breakTime;
+      timer.state.status = 'break';
+      timer.buildTimeline();
+
+      const target = timer.timeline[timer.state.period] + 1000;
+      timer.sync(target);
+
+      expect(timer.state.period).toBe(2);
+      expect(timer.state.time).toBe(timer.settings.cycleTime);
+      expect(timer.state.status).toBe('initial');
+    });
   });
 });
