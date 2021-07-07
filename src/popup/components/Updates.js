848f4283f646c8928528519ae93df5915ebe9cd2
Diego Serrano <diegoserranor@gmail.com>
2021-07-07 14:10:35 -0500

    Version bump, and update to release items

--- src/popup/components/Updates.js
+++ src/popup/components/Updates.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-import useLocale from "../hooks/useLocale";
+import useLocale from '../hooks/useLocale';
 
-import Section from "./Common/Section";
+import Section from './Common/Section';
 
 const List = styled.ul`
   list-style-position: outside;
@@ -18,11 +18,10 @@ const Item = styled.li`
 `;
 
 const locale_set = [
-  "updates",
-  "updates_greeting",
-  "updates_notes",
-  "updates_update_1",
-  "updates_update_2",
+  'updates',
+  'updates_greeting',
+  'updates_notes',
+  'updates_update_1',
 ];
 
 const Updates = () => {
@@ -30,13 +29,13 @@ const Updates = () => {
 
   return (
     <Section>
-      <h1>{locale["updates"]}</h1>
-      <p>{locale["updates_greeting"]}</p>
+      <h1>{locale['updates']}</h1>
+      <p>{locale['updates_greeting']}</p>
 
-      <span>{locale["updates_notes"]}</span>
+      <span>{locale['updates_notes']}</span>
       <List>
-        <Item>{locale["updates_update_1"]}</Item>
-        <Item>{locale["updates_update_2"]}</Item>
+        <Item>{locale['updates_update_1']}</Item>
+        {/* <Item>{locale["updates_update_2"]}</Item> */}
       </List>
     </Section>
   );
