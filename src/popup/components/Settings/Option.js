72e048d2dd5a5710b93b7472e3120f3d86c19dce
Diego Serrano <diegoserranor@gmail.com>
2021-07-07 13:09:41 -0500

    Add 'no-wrap' condition for timer badge property
    This is a temp fix for layout width differences in different locales

--- src/popup/components/Settings/Option.js
+++ src/popup/components/Settings/Option.js
@@ -1,28 +1,31 @@
-"use strict";
+'use strict';
 
-import React from "react";
-import styled from "styled-components";
-import Checkbox from "./Checkbox";
-import Number from "./Number";
+import React from 'react';
+import styled from 'styled-components';
+import Checkbox from './Checkbox';
+import Number from './Number';
 
 const StyledOption = styled.div`
+  max-width: 190px;
   display: grid;
   align-items: center;
   grid-template-columns: 1fr 40px;
-  column-gap: 15px;
-  margin-bottom: ${(props) => (props.margin ? "13px" : "0")};
-  white-space: nowrap;
+  column-gap: 10px;
+  margin-bottom: ${(props) => (props.margin ? '13px' : '0')};
+  white-space: ${(props) => (props.noWrap ? 'normal' : 'nowrap')};
 `;
 
 const Input = ({ type, storage }) => {
-  if (type === "checkbox") return <Checkbox storage={storage} />;
-  else if (type === "number") return <Number storage={storage} />;
+  if (type === 'checkbox') return <Checkbox storage={storage} />;
+  else if (type === 'number') return <Number storage={storage} />;
   else return null;
 };
 
 const Option = ({ option, last }) => {
+  const noWrap = option.storage === 'badgeTimer';
+
   return (
-    <StyledOption margin={!last}>
+    <StyledOption margin={!last} noWrap={noWrap}>
       <span>{option.name}</span>
       <Input type={option.type} storage={option.storage} />
     </StyledOption>
